1f8f82dabecde3e3f24182f3b7fe93576bf1ff32
Yona1201 <149564745+Yona1201@users.noreply.github.com>
2024-06-20 16:32:36 +0700

    commit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('jenis_kelamin', ['l', 'p']);
+            $table->string('phone')->unique();
+            $table->integer('role');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
