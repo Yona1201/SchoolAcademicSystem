edf62f4a03584c3a3771fd8958677f209f1526f1
Yona1201 <149564745+Yona1201@users.noreply.github.com>
2024-06-20 20:13:40 +0700

    delete

--- resources/views/components/button-modal-component.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['color' => 'btn-primary', 'idmodal' => 'modal', 'disabled' => false])
-
-<button type="button" class="btn {{ $color }}" {{ $disabled ? 'disabled' : '' }} data-bs-toggle="modal"
-		data-bs-target="#{{ $idmodal }}">
-		{{ $slot }}
-</button>
